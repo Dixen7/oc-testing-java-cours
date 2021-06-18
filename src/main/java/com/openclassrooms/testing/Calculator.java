6df0b2677d6dae2ee375456943ae3efc12b35d4b
Dixen7 <dixenlive@gmail.com>
2021-06-18 14:54:31 +0200

    add testUnitaire AssertEqual/AssertTrue

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -6,4 +6,15 @@ public class Calculator {
 		return a + b;
 	}
 
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public void longCalculation() {
+		try {
+			Thread.sleep(500);
+		} catch (final InterruptedException e){
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -2,22 +2,101 @@ package com.openclassrooms.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
 
-class CalculatorTest {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTest {
+	
+	private Calculator calculatorUnderTest;
+	private static Instant startedAt;
+	
+	@BeforeAll
+	public static void initStartingTime() {
+		System.out.println("Appel avant tous les tests");
+		startedAt = Instant.now(); 
+	}
+	
+	@AfterAll
+	public static void showTestDuration() {
+		System.out.println("Appel aprés tous les tests");
+		Instant endedAt = Instant.now();
+		long duration = Duration.between(startedAt, endedAt).toMillis();
+		System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+	}
+	
+	@BeforeEach
+	public void initCalculator() {
+		calculatorUnderTest = new Calculator();
+		System.out.println("Appel avant chaque test");
+	}
+	
+	@AfterEach
+	public void undefCalculator() {
+		System.out.println("Appel aprés chaque test");
+		calculatorUnderTest = null;
+	}
 
 	@Test
-	void testAddTwoPositiveNumbers() {
+	public void testAddTwoPositiveNumbers() {
 		// Arrange
 		int a = 2;
 		int b = 3;
-		Calculator calculator = new Calculator();
-
 		// Act
-		int somme = calculator.add(a, b);
-
+		int somme = calculatorUnderTest.add(a, b);
 		// Assert
 		assertEquals(5, somme);
 	}
+	
+	@Test 
+	public void multiplyTwoNumbers() {
+		int a = 3;
+		int b = 3;
+		int produit = calculatorUnderTest.multiply(a, b);
+		
+		assertEquals(9, produit);
+	}
+	
+	@ParameterizedTest(name = "{0} x 0 doit être égal à 0")
+	@ValueSource(ints = {1, 2, 42, 1001, 5089})
+	public void multiply_shouldReturnZero(int arg) {
+		// ARRANGE tout est prêt !
+		
+		// ACT Multiplier par zéro
+		final int actualResult = calculatorUnderTest.multiply(arg, 0);
+		//ASSERT ça vaut toujours zéro ! 
+		assertEquals(0, actualResult);
+	}
+	
+	@ParameterizedTest(name = "{0} + {1} doit être égal à {2}")
+	@CsvSource({"1,1,2","3,2,5","100,50,150"})
+	public void multiply_shouldReturnTheSum(int arg1, int arg2, int expectResult) {
+		// ARRANGE tout est prêt !
+		
+		// ACT
+		final int actualResult = calculatorUnderTest.add(arg1, arg2);
+		//ASSERT
+		assertEquals(expectResult, actualResult);
+	}
+	
+	@Test
+	@Timeout(1)
+	public void longCalcul_computeIn1second() {
+		// ARRANGE
+		
+		// ACT
+		calculatorUnderTest.longCalculation();
+		//ASSERT
 
+	}
 }
